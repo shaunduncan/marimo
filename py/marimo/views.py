54eef9ec3ca8261ed24c53594779845a6f0ee497
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-26 15:35:17 -0400

    first pass at router

---
+++ py/marimo/views.py
@@ -0,0 +1,38 @@
+import json
+
+from django.conf import settings
+from django.http import Http404, HttpResponse
+from django.views.generic.base import View
+
+from utils import smart_import
+
+_marimo_widgets = __import__(settings.MARIMO_MODULE).registry
+
+
+class MarimoRouter(View):
+    def get(self, request):
+        try:
+            bulk = json.loads(request.GET.get['bulk'])
+        except KeyError:
+            raise Http404()
+        else:
+            return self.route(bulk)
+
+    def route(self, bulk):
+        response = []
+        # TODO sanitize bulk
+        for widget in bulk:
+            # Try to get a callable from the dict... if it's not imported deal with it
+            view = _marimo_widgets[widget['widget_name']]
+            if not callable(view):
+                view = smart_import(view)
+                _marimo_widgets[widget['widget_name']] = view
+            data = { 'div_id': widget['div_id'], }
+            try:
+                data.update(view(*bulk[args], **bulk[kwargs])) 
+            except: #TODO: needs better error handling
+                data['status'] = 'failed'
+            else:
+                data['status'] = 'succeeded'
+            response.append(data)
+        return HttpResponse(json.dumps(response), mimetype='application/json')
