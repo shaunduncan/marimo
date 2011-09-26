c010bb355890049e0006f5b26de499b74d1a194c
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-26 11:15:35 -0400

    a very simple test passes

--- py/marimo/tests.py
+++ py/marimo/tests.py
@@ -12,6 +12,7 @@ class TestTag(TestCase):
         request = mock.Mock()
         request.META = dict(PATH_INFO='/some/path')
         self.context['request'] = request
+        self.expected1 = """ <div class="marimo class" data-murl="/thissite" data-json="%7B%22resolved_kwargs%22%3A%20%7B%22k2%22%3A%20%22stringkwarg%22%2C%20%22k1%22%3A%20%22incon%22%7D%2C%20%22args%22%3A%20%5B%22incon%22%2C%20%22stringarg%22%5D%2C%20%22widget_name%22%3A%20%22test%22%7D"></div>"""
 
     def tearDown(self):
         if self.real_murl is None:
@@ -23,4 +24,4 @@ class TestTag(TestCase):
         t = template.Template("""{% load marimo %} {% marimo test incontext "stringarg" k1=incontext k2="stringkwarg" %}""")
         self.context['incontext'] = 'incon'
         rendered = t.render(self.context)
-        print rendered
+        self.assertEqual(rendered, self.expected1)
