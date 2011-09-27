ba412c02715f4262bae816abdce487942fa98050
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-27 11:22:46 -0400

    rewrite js. is now more extensible and hopefully works

--- js/marimo.js
+++ js/marimo.js
@@ -2,116 +2,101 @@
 // TODO don't assume JSON
 // TODO fix indentation
 function Widget(data) {
+    // TODO div_id -> widget_id
+    this.id = data.div_id;
     // TODO write
+    this.args = data;
+    this.data = {};
 }
+
+Widget.prototype.update = function(data) {
+    // merge data with this's
+    // TODO make this proper update, for now overwrite
+    this.data = data;
+    this.render();
+    // TODO future excitement
+};
+
+Widget.prototype.render = function() {
+    function finish(template, context) {
+        // TODO make not-mustache-specific
+        var html = Mustache.to_html(template, context);
+        $('#'+this.id).html(html);
+    }
+    if (this.data.template_url) {
+        // TODO fetch, pass finish as callback
+    }
+    else { finish(this.data.template, this.data.context); }
+};
+
+
+function BatchRequest(url) {
+    this.payloads = [];
+    this.url = url;
+};
+
+BatchRequest.prototype.add = function(payload) {
+    // TODO make more resilient/less trusting in the face of corrupted div_ids etc
+    this.payloads.push(payload);
+};
+
+BatchRequest.prototype.make_request = function(cb) {
+   $.ajax(this.url, {
+     data: {bulk:JSON.stringify(this.payloads)},
+     dataType: 'json',
+     success: function(data) { cb(url, data); },
+     error: function() {
+       console.log('error processing bulk request '+this.url+' with json '+JSON.stringify(this.payloads));
+     }
+   });
+};
+
+
 function Marimo() {
     this.requests = {};
     this.widgets = {};
+    this.widgets_in = 0;
 }
-Marimo.prototype.add_widget = function(widget_data) {
-    var widget = new Widget(widget_data);
-    if (!this.requests[widget.murl]) {
-        this.requests[widget.murl] = [widget];
+
+Marimo.prototype.add_widget = function(widget_args) {
+    // TODO lock request objs while they are making a request
+    this.widgets_in++;
+    setTimout(function() {
+        var url = widget_args.murl;
+        if (!this.requests[url]) {
+            this.requests[url] = new BatchRequest(url);
+        }
+        this.requests[url].add(widget_args);
+        var widget = new Widget(widget_args);
+        this.widgets[widget.id] = widget;
+        this.widgets_in--;
+   }, 1);
+};
+
+Marimo.prototype.make_request = function() {
+    if (this.widgets_in > 0) {
+        setTimeout(this.make_request, 100);
+        return;
     }
-    else {
-        this.requests[widget.murl].push(widget);
+    for (var key in this.requests) {
+        if (!this.requests.hasOwnProperty(key)) { return; }
+        var batch = this.requests[key];
+        batch.make_request(this.handle_response);
     }
 };
 
-Marimo.prototype.bulk_request = function(requests) {
-    requests = requests || this.requests;
-    for (var url in requests) {
-        if (!requests.hasOwnProperty(url)) { return; }
-        $.ajax(url, {
-          data: {bulk:requests[url]},
-          dataType: 'json',
-          success: this.handle_bulk,
-          error: function() {
-            console.log('error processing bulk request '+url+' with json '+JSON.stringify(requests[url]));
-          }
-        });
+Marimo.prototype.handle_response = function(url, data) {
+    delete this.requests[url];
+    for (var key in this.widgets) {
+        if (!this.widgets.hasOwnProperty(key)) {return;}
+        var widget = this.widgets[key];
+        widget.update(data);
     }
-};
-
-Marimo.prototype.handle_bulk = function(data) {
-      for (var key in data) {
-        if (!data.hasOwnProperty(key)) {return;}    
-        var widget_json = data[key];
-        var widget = new Widget(JSON.parse(widget_json));
-        render(widget, function(err, html) {
-            // TODO pay attention to err
-            var div_id = widget.div_id;
-            $('#'+div_id).html(html);
-        });
-      }
+    // tell widgets to update based on what is in data.
 };
 
 var marimo = new Marimo();
 
 $(function() {
-    // we have marimo this.widgets
-    // TODO marimo object to store state; don't tie stuff to DOM
-    var bulk = {};
-    $('div.marimo').each(function() {
-      // care about url and json data
-      var url = $(this).attr('data-murl');
-      console.log('encoded: ' + $(this).attr('data-json'));
-      console.log('decoded: ' + decodeURI($(this).attr('data-json')));
-      console.log('parsed: ' + JSON.parse(decodeURI($(this).attr('data-json'))));
-      var widget = {
-        json: decodeURI($(this).attr('data-json')),
-        div: this
-      };
-      // TODO what if data-* doesn't exist? check and skip the div
-      if (!bulk[url]) {
-        bulk[url] = [];
-      }
-      else {
-        bulk[url].push(widget);
-      }
-    });
-
-    // TODO give obj a meaningful name
-    function render(obj, cb) {
-        // TODO accept a render function that will override finish()
-        // template
-        // template_url
-        // context
-        function finish(template, context, cb) {
-            // TODO make not-mustache-specific
-            var html = Mustache.to_html(template, context);
-            cb(null, html);
-        }
-        if (obj.template_url) {
-            // fetch, pass finish as callback
-        }
-        else { finish(obj.template, obj.context, cb); }
-    }
-
-    function handle_bulk(data) {
-      // data is json
-      for (var key in data) {
-        if (!data.hasOwnProperty(key)) {return;}    
-        var widget = data[key];
-        render(widget, function(err, html) {
-            // TODO pay attention to err
-            var div_id = widget.div_id;
-            $('#'+div_id).html(html);
-        });
-      }
-    }
-
-    for (var url in bulk) {
-      if (!bulk.hasOwnProperty(url)) { return; }
-      var widgets_json = JSON.stringify(bulk[url]);
-      // TODO x-domain?
-      $.ajax(url, {
-        data: {bulk:widgets_json},
-        dataType: 'json',
-        success: handle_bulk,
-        error: function() {
-          console.log('error processing bulk request '+url+' with json '+widgets_json);
-        }
-      });
-    }
+    marimo.make_request();
 });
