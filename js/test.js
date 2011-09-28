5334b58e7039ce85a63a44453df4349cfc9bce18
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-28 13:26:56 -0400

    write a working test runner for browserjs in node

--- js/test.js
+++ js/test.js
@@ -1,17 +1,35 @@
 var assert = require('assert');
 var jsdom = require('jsdom');
 
-jsdom.env(
-    '<html><head></head><body><div id="test_widget">hi</div></body>', [
-        './jquery-1.6.4.js',
-        './mustache.js',
-        './marimo.js'
-    ],
-    run_tests
-);
+setTimeout = function(fun) { fun(); };
 
-function run_tests(errors, window) {
-    var $ = window.$;
-    console.log(window.marimo);
-    assert.equal($('#test_widget').html(), 'hi', 'found test_widget html');
+function TestCase() {
+    this.tests = [];
 }
+
+TestCase.prototype.run = function() {
+    this.tests.forEach(function(test){
+        var testcase = {};
+        jsdom.env(
+            '<html><head></head><body><div id="test_widget">hi</div></body>', [
+                './jquery-1.6.4.js',
+                './mustache.js',
+                './marimo.js'
+            ],
+            function(err, window) { testcase.window = window; test(testcase); }
+        );
+    }, this);
+};
+
+TestCase.prototype.add = function(testfun) {
+    this.tests.push(testfun);
+};
+
+var marimo_obj_tc = new TestCase();
+marimo_obj_tc.add(function(testcase) {
+    var window = testcase.window;
+    var marimo = window.marimo;
+    marimo.add_widget({murl:"/some/url", div_id:"123"});
+    assert.ok(marimo.widgets['123'], 'widget 123 added to marimo.widgets');
+});
+marimo_obj_tc.run();
