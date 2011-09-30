ff2e1d27082110074a03b0206d36279bb66e51f6
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-30 13:30:51 -0400

    syntax error corrected

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = "marimo",
     version = "0.1",
     packages = ['marimo'],
-    package_dir = {'': 'py'}
+    package_dir = {'': 'py'},
     author = "Cox Media Group",
     author_email = "opensource@coxinc.com",
     description = "a fast framework for asynchronous widgets",
