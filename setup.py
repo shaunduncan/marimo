573128fa00864e69ae93cf4cc905f213aa285381
Nate Smith <nate.smith@coxinc.com>
2011-09-30 14:03:34 -0400

    try py_modules

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     version = "0.1",
     packages = find_packages('py'),
     package_dir = {'': 'py'},
+    py_modules = ['marimo', 'marimo.views'],
     author = "Cox Media Group",
     author_email = "opensource@coxinc.com",
     description = "a fast framework for asynchronous widgets",
