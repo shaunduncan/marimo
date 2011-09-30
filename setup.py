17abad0f0ba55b034eeb01364253962617bfbdc4
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-30 13:27:14 -0400

    setup.py added must commit to test

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name = "marimo",
+    version = "0.1",
+    packages = ['marimo'],
+    package_dir = {'': 'py'}
+    author = "Cox Media Group",
+    author_email = "opensource@coxinc.com",
+    description = "a fast framework for asynchronous widgets",
+    license = "MIT",
+    url = "https://github.com/coxmediagroup/marimo",
+)
+
