d91100ee6b81d7d846363d5c834842d91f66066b
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-30 13:50:37 -0400

    removed find_packages()

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name = "marimo",
     version = "0.1",
-    packages = find_packages(),
+    packages = ['marimo'],
     package_dir = {'': 'py'},
     author = "Cox Media Group",
     author_email = "opensource@coxinc.com",
