1473b018ac007d13d0e2a2f383705353131f40a1
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-30 14:20:08 -0400

    removed py_modules

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     version = "0.1",
     packages = find_packages(exclude='mtest'),
     package_dir = {'': 'py'},
-    py_modules = ['marimo.views'],
     author = "Cox Media Group",
     author_email = "opensource@coxinc.com",
     description = "a fast framework for asynchronous widgets",
