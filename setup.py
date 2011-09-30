35362bca6dd14d0ceeb70bbc5e83cd1a5e950f05
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-30 13:57:40 -0400

    one more try

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 setup(
     name = "marimo",
     version = "0.1",
-    packages = ['py'],
+    packages = find_packages('py'),
+    package_dir = {'': 'py'},
     author = "Cox Media Group",
     author_email = "opensource@coxinc.com",
     description = "a fast framework for asynchronous widgets",
