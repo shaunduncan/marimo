92f7eed76a77c823115933e63f8747885ef93827
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-09-30 11:11:44 -0400

    test router view

--- py/marimo/tests.py
+++ py/marimo/tests.py
@@ -1,9 +1,19 @@
+import json
 import re
+
 from django.conf import settings
+from django.http import Http404, HttpRequest
 from django.utils.unittest import TestCase
 from django import template
+
 import mock
 
+from marimo.views import MarimoRouter
+
+widgets = {
+        'test': lambda x,y,z: {'key':'value'},
+        'failure': lambda x,y: 1
+}
 
 class TestTag(TestCase):
     def setUp(self):
@@ -28,3 +38,47 @@ class TestTag(TestCase):
         rendered = t.render(self.context)
         self.assertTrue(re.search(r'<div id="test_.+" class="marimo class"', rendered))
         self.assertEquals(len(self.context['marimo_widgets']), 1)
+
+class TestRouterView(TestCase):
+    def setUp(self):
+        self.router = MarimoRouter()
+
+    def tearDown(self):
+        pass
+
+    def test_get(self):
+        self.assertRaises(Http404, self.router.get, HttpRequest())
+
+    @mock.patch('marimo.views.router._marimo_widgets', widgets)
+    @mock.patch('marimo.views.router.HttpResponse')
+    def test_route_success(self, http_response):
+        bulk = [
+                {'id':'1', 'widget_name':'test', 'args':['one', 'two'], 'kwargs':{}}
+        ]
+        self.router.route(None, bulk)
+        response = json.loads(http_response.call_args[0][0])
+        self.assertEqual(len(response), 1)
+        self.assertEqual(response[0]['status'], 'succeeded')
+        self.assertEqual(response[0]['id'], '1')
+
+    @mock.patch('marimo.views.router._marimo_widgets', widgets)
+    @mock.patch('marimo.views.router.HttpResponse')
+    def test_route_callable_fails(self, http_response):
+        bulk = [
+                {'id':'1', 'widget_name':'failure', 'args':['one', 'two'], 'kwargs':{}}
+        ]
+        self.router.route(None, bulk)
+        response = json.loads(http_response.call_args[0][0])
+        self.assertEquals(response[0]['status'], 'failed')
+
+    @mock.patch('marimo.views.router._marimo_widgets', widgets)
+    @mock.patch('marimo.views.router.HttpResponse')
+    def test_route_no_such_widget(self, http_response):
+        bulk = [
+                {'id':'1', 'widget_name':'nopechucktesta', 'args':['one', 'two'], 'kwargs':{}}
+        ]
+        self.router.route(None, bulk)
+        response = json.loads(http_response.call_args[0][0])
+        self.assertEquals(response[0]['status'], 'WidgetNotFound')
+
+    # TODO test fetching a callable with smart_import. this is too gnarly for now.
