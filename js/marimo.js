3e37ae4a848dd5c91c03f29d35a1dfb81de32d36
Nate Smith <nate.smith@coxinc.com>
2011-10-13 12:54:33 -0400

    first pass at namespacing/object cleanup

--- js/marimo.js
+++ js/marimo.js
@@ -2,8 +2,85 @@
 // TODO don't assume JSON
 // TODO fix indentation
 
+// namespace
+// stores marimo state, acts as event broker for widgets,
+var marimo = {
+    requests: {},
+    widgets: {},
+    events: {},
+    init: function init($) {
+        this.$ = $;
+        return this;
+    },
+    add_widget: function add_widget(widget_args) {
+       var widget_prototype = this.widgetlib[widget_args['widget_prototype']];
+       console.log('TRYING TO ADD WIDGET WITH ID '+ widget_args['id']);
+       console.log('GOT WIDGET_PROTOTYPE: ' + widget_args['widget_prototype']);
+       if (!widget_prototype) {
+           widget_prototype = this.widgetlib.base_widget;
+       }
+       var w = Object.create(widget_prototype);
+       this.widgets[widget_args.id] = w;
+       this.widgets[widget_args.id].init(widget_args);
+    },
+    add_widgets: function add_widgets(widgets) {
+        console.log(widgets);
+        for (var key in widgets) {
+            if (!widgets.hasOwnProperty(key)){return;}
+            this.add_widget(widgets[key]);
+        }
+    },
+    make_request: function make_request() {
+        for (var key in this.requests) {
+            if (!this.requests.hasOwnProperty(key)) { return; }
+            var batch = this.requests[key];
+            var that = this;
+            batch.make_request(function(url, data) { that.handle_response(url, data) });
+        }
+    },
+    handle_response: function handle_response(url, data) {
+        delete this.requests[url];
+        for (var datum in data) {
+            if (!data.hasOwnProperty(datum)) {return}
+            var widget_data = data[datum];
+            this.widgets[widget_data.id].update(widget_data);
+        }
+        // tell widgets to update based on what is in data.
+    },
+    emit: function emit(evnt) {
+        this.events[evnt] = true;
+        this.$(document).trigger(evnt);
+    }
+};
+
+marimo.batch_request = {
+    payloads: [],
+    init: function init(url) {
+        this.url = url;
+        return this;
+    },
+    add: function add(payload) {
+        // TODO make more resilient/less trusting in the face of corrupted ids etc
+        this.payloads.push(payload);
+    },
+    make_request: function make_request(cb) {
+       var that = this;
+       marimo.$.ajax({
+         url: this.url,
+         data: {bulk:JSON.stringify(this.payloads)},
+         dataType: 'json',
+         success: function(data) { cb(that.url, data); },
+         error: function() {
+           console.log('error processing bulk request '+that.url+' with json '+JSON.stringify(that.payloads));
+         }
+       });
+    }
+};
+
+marimo.widgetlib = {};
+
 // this is a base widget that knows nothing about requests
-var widget = {
+marimo.widgetlib.base_widget = {
     init: function(data) {
         this.id = data.id;
         this.data = data;
@@ -21,13 +98,21 @@ var widget = {
 };
 
 // this is a requestful widget that uses network calls to get templates/info to render
-var request_widget = Object.create(widget);{
-request_widget.init = function(data) {
+marimo.widgetlib.request_widget = Object.create(marimo.widgetlib.base_widget);
+marimo.widgetlib.request_widget.init = function(data) {
     this.id = data.id;
+    this.url = data.url;
     this.data = data;
+    this.add_request();
     return this;
 };
-request_widget.update = function(data) {
+marimo.widgetlib.request_widget.add_request = function add_request() {
+    if (!marimo.requests[this.url]) {
+        marimo.requests[this.url] = Object.create(marimo.batch_widget).init(this.url);
+    }
+    marimo.requests[this.url].add(this.data);
+};
+marimo.widgetlib.request_widget.update = function(data) {
     // merge data with this's
     for (var key in data) {
         if (!data.hasOwnProperty(key)) { return; }
@@ -36,7 +121,7 @@ request_widget.update = function(data) {
     this.render();
     // TODO future excitement
 };
-request_widget.render = function() {
+marimo.widgetlib.request_widget.render = function() {
     // TODO support a template_url
     // TODO make not-mustache-specific
     var html = Mustache.to_html(this.data.template, this.data.context);
@@ -49,86 +134,3 @@ request_widget.render = function() {
 };
 
 // TODO websocket widget
-
-function BatchRequest(url) {
-    this.payloads = [];
-    this.url = url;
-};
-
-BatchRequest.prototype.add = function(payload) {
-    // TODO make more resilient/less trusting in the face of corrupted ids etc
-    this.payloads.push(payload);
-};
-
-BatchRequest.prototype.make_request = function(cb) {
-   var that = this;
-   marimo.$.ajax({
-     url: this.url,
-     data: {bulk:JSON.stringify(this.payloads)},
-     dataType: 'json',
-     success: function(data) { cb(that.url, data); },
-     error: function() {
-       console.log('error processing bulk request '+that.url+' with json '+JSON.stringify(that.payloads));
-     }
-   });
-};
-
-
-function Marimo($) {
-    this.requests = {};
-    this.widgets = {};
-    this.widgets_in = 0;
-    this.events ={};
-    this.$ = $;
-}
-
-Marimo.prototype.add_widget = function(widget_args) {
-   var widget_prototype = window[widget_args['widget_prototype']];
-   console.log('TRYING TO ADD WIDGET WITH ID '+ widget_args['id']);
-   console.log('GOT WIDGET_PROTOTYPE: ' + widget_args['widget_prototype']);
-   if (!widget_prototype) {
-       widget_prototype = widget;
-   }
-   var w = Object.create(widget_prototype);
-   this.widgets[widget_args.id] = w;
-   this.widgets[widget_args.id].init(widget_args);
-};
-
-Marimo.prototype.add_widgets = function(widgets) {
-    console.log(widgets);
-    for (var key in widgets) {
-        if (!widgets.hasOwnProperty(key)){return;}
-        this.add_widget(widgets[key]);
-    }
-};
-
-Marimo.prototype.make_request = function() {
-    if (this.widgets_in > 0) {
-        var that = this;
-        setTimeout(function() { that.make_request() }, 1);
-        return;
-    }
-    for (var key in this.requests) {
-        if (!this.requests.hasOwnProperty(key)) { return; }
-        var batch = this.requests[key];
-        var that = this;
-        batch.make_request(function(url, data) { that.handle_response(url, data) });
-    }
-};
-
-Marimo.prototype.handle_response = function(url, data) {
-    delete this.requests[url];
-    for (var datum in data) {
-        if (!data.hasOwnProperty(datum)) {return}
-        var widget_data = data[datum];
-        this.widgets[widget_data.id].update(widget_data);
-    }
-    // tell widgets to update based on what is in data.
-};
-
-Marimo.prototype.emit = function(evnt) {
-    this.events[evnt] = true;
-    this.$(document).trigger(evnt);
-};
-
-var marimo = new Marimo();
