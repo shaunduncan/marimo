e9c529a270323983e37c5f56b2b7e745cb0c2c72
Nate Smith <nate.smith@coxinc.com>
2012-05-04 13:05:19 -0400

    finish testing marimo object

--- test/test.marimo_obj.js
+++ test/test.marimo_obj.js
@@ -16,6 +16,23 @@ exports.test_marimo_obj = {
         var widget = marimo.widgets[keys[0]]
 
         test.ok(widget.id, 'widget has an id')
+
+        test.done()
+    },
+    test_add_widget: function(test) {
+        var marimo = this.window.marimo
+
+        var widget_args ={
+            id: 'onetwothree',
+            widget_prototype: 'request_widget',
+            arbitrary: 'data'
+        }
+        marimo.add_widget(widget_args)
+        var widget = marimo.widgets.onetwothree
+        test.ok(widget, 'widget was created')
+        test.equal(widget.data.arbitrary, 'data', 'initialization data preserved')
+        test.ok(widget.data.widget_prototype !== 'base_widget', 'did not fall back to base widget')
+
         test.done()
     },
     test_add_widgets: function(test) {
@@ -31,4 +48,29 @@ exports.test_marimo_obj = {
 
         test.done()
     },
+    test_printf: function(test) {
+        var marimo = this.window.marimo
+        var formatted = marimo.printf('hello, %s. are you %s?', ['geoffery', 'jellinek'])
+        test.equal('hello, geoffery. are you jellinek?', formatted, 'formatting is correct')
+        test.done()
+    },
+    test_extend: function(test) {
+        var marimo = this.window.marimo
+        var obja = {
+            init: function(){ return this },
+            do_stuff: function(){ return 'stuff' },
+            propa: 'a'
+        }
+        var objb = marimo.extend(obja, {
+            b_stuff: function b_stuff() { return 'bstuff' },
+            propb: 'b'
+        })
+        test.ok(objb, 'object returned')
+        test.equal(objb.propa, 'a', 'can get to obja stuff')
+        test.equal(objb.propb, 'b', 'can get to new prop')
+        test.equal(objb.b_stuff.name, 'b_stuff', 'function name preserved')
+
+        test.done()
+    }
+    // not testing random_int.
 }
